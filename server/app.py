fd27a3d4ae55c9799bcee2d8d71a9baaf0e2d313
Abdulqdir <yarefaadi@gmail.com>
2021-12-06 10:35:16 -0800

    app.py set up

--- server/app.py
+++ server/app.py
@@ -1,11 +1,31 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from server.module import *
 #create flask instance
 app = Flask(__name__)
 # add database
-app.config['SQLALCHEMY_DATABASE_URI'] = "database path/url"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://gtbbojbdpfuvny:d763d0bf441b5a29c4fa6542b26502e7934ea733ad4bfcb02d7903bcd7affca6@ec2-3-95-130-249.compute-1.amazonaws.com:5432/d7s9m35lp1c3ph"
 # create tables/intialise the database
 db = SQLAlchemy(app)
+#user id
+user_id = 330
+
+# add user to the database
+@app.route('/add_user', methods = ['post'])
+def add_user():
+    req = request.json
+    first_name = req.get('first_name')
+    last_name = req.get('last_name')
+    email = req.get('email')
+    user_name = req.get('user_name')
+    password = req.get('password')
+
+    result = USERS(user_id=user_id, first_name=first_name, last_name=last_name, email = email, user_name=user_name, password = password)
+    db.session.add(result)
+    db.session.commit()
+    return 'information was added!'
+
 if __name__ == '__main__':
-    print("hello")
+    row = db.engine.execute('SELECT recipe_title, rating FROM RECIPE FULL OUTER JOIN RATING ON RECIPE.recipe_id = RATING.recipe_id;').first()
+    print(row)
--- server/module.py
+++ server/module.py
@@ -3,27 +3,27 @@ from datetime import datetime
 class RECIPE(db.Model):
     recipe_id = db.Column(db.Integer, primary_key = True, nullable = False)
     recipe_title = db.Column(db.String(200), nullable = False)
-    meal_id = db.Column(db.Integer,db.ForeignKey("MEAL_TYPE.meal_id", ondelete="SET NULL"), server_default = '7')
+    meal_id = db.Column(db.Integer)
     cost = db.Column(db.Integer, nullable = False)
     prep_time = db.Column(db.String(200), nullable = False, server_default = "5 minutes")
-    created_user_id = db.Column(db.Integer, db.ForeignKey("USER.user_id", ondelete="SET NULL") ,nullable = False, server_default = '0')
+    created_user_id = db.Column(db.Integer ,nullable = False, server_default = '0')
     recipe_description = db.Column(db.String(200), nullable = False, server_default = "No description provided")
     instructions = db.Column(db.String(200), nullable = False, server_default = "No description provided")
     image_url = db.Column(db.String(200))
     date = db.Column(db.DateTime, default = datetime.utcnow)
 
-class User(db.Model):
+class USERS(db.Model):
     user_id = db.Column(db.Integer, primary_key = True, nullable = False)
     first_name = db.Column(db.String(200), nullable = False)
     last_name = db.Column(db.String(200), nullable = False)
-    user_name = db.Column(db.String(200), db.CheckConstraint('user_name>4'),nullable = False, unique = True)
-    email = db.Column(db.String(200), nullable = False, unique = True)
-    password = db.Column(db.Text, db.CheckConstraint('password>8'), nullable = False, unique = True)
+    user_name = db.Column(db.String(200), nullable = False)
+    email = db.Column(db.String(200), nullable = False)
+    password = db.Column(db.Text, nullable = False)
 
 class CONSISTS_OF(db.Model):
-    recipe_id = db.Column(db.Integer, db.ForeignKey("RECIPE.recipe_id", ondelete="CASCADE"),primary_key = True, nullable = False)
-    ingredient_id = db.Column(db.Integer, db.ForeignKey("INGREDIENT.ingredient_id", ondelete="SET NULL"),nullable = False , primary_key = True)
-    quantity = db.Column(db.Float, db.CheckConstraint('quantity>0'))
+    recipe_id = db.Column(db.Integer, primary_key = True, nullable = False)
+    ingredient_id = db.Column(db.Integer,nullable = False , primary_key = True)
+    quantity = db.Column(db.Float)
     measurement = db.Column(db.String(200), nullable = False)
 
 
@@ -38,6 +38,6 @@ class INGREDIENT(db.Model):
 
 
 class RATING(db.Model):
-    recipe_id = db.Column(db.Integer, db.ForeignKey("RECIPE.recipe_id", ondelete="CASCADE"),primary_key = True, nullable = False)
-    user_id = db.Column(db.Integer, db.ForeignKey("USER.user_id", ondelete="SET NULL"),nullable = False)
-    rating = db.Column(db.Integer, db.CheckConstraint('rating>=0 and rating<=5'),server_default = '0')
+    recipe_id = db.Column(db.Integer, primary_key = True, nullable = False)
+    user_id = db.Column(db.Integer,nullable = False)
+    rating = db.Column(db.Integer, server_default = '0')
