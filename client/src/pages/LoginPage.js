9236b925477b594bd3413ae596be06852c05055c
lammai <lam7.2521@gmail.com>
2021-12-07 00:40:12 -0800

    modified:   client/package.json 	modified:   client/src/pages/LoginPage.js

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,26 +1,35 @@
-import { React, useState } from "react";
-import httpClient from "../httpClient";
+import { React, useState, useEffect } from "react";
+// import httpClient from "../httpClient";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const logInUser = async () => {
+  const logInUser = () => {
     console.log(email, password);
 
-    try {
-      const resp = await httpClient.get("//localhost:5000/login", {
-        email,
-        password,
-      });
+    // Axios stuff
+    // try {
+    //   const resp = await httpClient.get("//localhost:5000/login", {
+    //     email,
+    //     password,
+    //   });
 
-      window.location.href = "/"; //return to landing page if login succeed
-    } catch (error) {
-      if (error.resp.status === 401) {
-        alert("Invalid Credential");
-      }
-    }
+    //   window.location.href = "/"; //return to landing page if login succeed
+    // } catch (error) {
+    //   if (error.resp.status === 401) {
+    //     alert("Invalid Credential");
+    //   }
+    // }
   };
+
+  useEffect(() => {
+    fetch("/login").then((resp) =>
+      resp.json().then((data) => {
+        console.log(data);
+      })
+    );
+  }, []);
 
   return (
     <div style={{ padding: "20px" }}>
