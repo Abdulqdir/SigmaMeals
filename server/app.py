25436786b70d2e732efe071e392f2ef9acfc8758
Abdulqdir <yarefaadi@gmail.com>
2021-12-06 21:06:10 -0800

    added check user to app.py

--- server/app.py
+++ server/app.py
@@ -8,7 +8,7 @@ import os
 app = Flask(__name__)
 # add database
 app.config['SECRET_KEY'] = 'secret'
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://://gtbbojbdpfuvny:d763d0bf441b5a29c4fa6542b26502e7934ea733ad4bfcb02d7903bcd7affca6@ec2-3-95-130-249.compute-1.amazonaws.com:5432/d7s9m35lp1c3ph"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://gtbbojbdpfuvny:d763d0bf441b5a29c4fa6542b26502e7934ea733ad4bfcb02d7903bcd7affca6@ec2-3-95-130-249.compute-1.amazonaws.com:5432/d7s9m35lp1c3ph"
 # create tables/intialise the database
 db = SQLAlchemy(app)
 #user id
@@ -29,7 +29,20 @@ def add_user():
     db.session.commit()
     return 'information was added!'
 
+# add user to the database
+@app.route('/check_user', methods = ['GET'])
+def check_user():
+    req = request.json
+    user_name = req.get('username')
+    password = req.get('password')
+    #result = USERS.query.filter_by(username=user_name, password = password).first()
+    result = db.engine.execute('SELECT username, password FROM USERS WHERE USERS.user_name = user_name AND USERS.password = password').first()
+    if result is None:
+        return "user doesn't exist"
+    else:
+        return result.user_name
+
 if __name__ == '__main__':
     app.debug=True
-    row = db.engine.execute('SELECT recipe_title, rating FROM RECIPE FULL OUTER JOIN RATING ON RECIPE.recipe_id = RATING.recipe_id;').first()
+    row = db.engine.execute('SELECT recipe_title, rating FROM RECIPE FULL OUTER JOIN RATING ON RECIPE.recipe_id = RATING.recipe_id').first()
     print(row)
