30e368e647450615f25717c7e6c45e74f0adb869
Abdulqdir <yarefaadi@gmail.com>
2021-12-06 22:43:29 -0800

    modified check user and add_user

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from module import *
 import os
@@ -7,16 +7,15 @@ import os
 #create flask instance
 app = Flask(__name__)
 # add database
-app.config['SECRET_KEY'] = 'secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://gtbbojbdpfuvny:d763d0bf441b5a29c4fa6542b26502e7934ea733ad4bfcb02d7903bcd7affca6@ec2-3-95-130-249.compute-1.amazonaws.com:5432/d7s9m35lp1c3ph"
 # create tables/intialise the database
 db = SQLAlchemy(app)
 #user id
 user_id = 330
-
+#why do we need to methods = ["post"]
 # add user to the database
-@app.route('/add_user', methods = ['post'])
-def add_user():
+@app.route('/create_user', methods = ['post'])
+def create_user():
     req = request.json
     first_name = req.get('first_name')
     last_name = req.get('last_name')
@@ -30,19 +29,19 @@ def add_user():
     return 'information was added!'
 
 # add user to the database
-@app.route('/check_user', methods = ['GET'])
-def check_user():
+@app.route('/login', methods = ['GET'])
+def login():
     req = request.json
     user_name = req.get('username')
     password = req.get('password')
     #result = USERS.query.filter_by(username=user_name, password = password).first()
-    result = db.engine.execute('SELECT username, password FROM USERS WHERE USERS.user_name = user_name AND USERS.password = password').first()
+    result = str(db.engine.execute('SELECT username, password FROM USERS WHERE USERS.user_name = {} AND USERS.password = {}'.format(user_name,password)).first())
     if result is None:
         return "user doesn't exist"
     else:
         return result.user_name
 
 if __name__ == '__main__':
-    app.debug=True
-    row = db.engine.execute('SELECT recipe_title, rating FROM RECIPE FULL OUTER JOIN RATING ON RECIPE.recipe_id = RATING.recipe_id').first()
+    row = str(db.session.execute('SELECT {}, rating FROM RECIPE FULL OUTER JOIN RATING ON RECIPE.recipe_id = RATING.recipe_id'.format("recipe_title")).first())
+    result = USERS(user_id=1111, first_name='Abdulqadir', last_name="Ibrahim", email = "email@gmail.com", user_name="king", password = "782754gh")
     print(row)
