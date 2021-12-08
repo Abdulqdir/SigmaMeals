31be85eadf4fdfeae81b0ae797d7f405734a7067
Artem Potafiy <apotafiy@uw.edu>
2021-12-07 21:09:30 -0800

    trying to get heroku to work

--- server/app.py
+++ server/app.py
@@ -103,5 +103,6 @@ def serve(path):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
     #print(jsonify(username="data",email="error",id="id"))
